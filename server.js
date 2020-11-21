f2f4ca3d9b6478d1fe8191926fdc6ed507d2f2ba
EthanRawlins <ethanrawlins@gmail.com>
2020-11-20 17:09:29 -0700

    Updates to geocode json file

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const app = express()
 const mysql      = require('mysql');
 const fs = require('fs')
 
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+
 const connection = mysql.createConnection({
     user: "feelthes_jared",
     password: "ullrich",
@@ -26,16 +29,43 @@ connection.query('SELECT ID, FirstName, LastName, PhoneNumber, StreetAddress, Un
         throw error;
     }
 
+    for (i = 0; i < results.length; i++) {
+        const add = addr_search(results[i].StreetAddress + ', ' + results[i].StateProvince);
+        results.geometry = add;
+    }
+
     const mapData = JSON.stringify(results);
 
-    fs.writeFile('./src/assets/contact.json', mapData, err => {
+    fs.writeFileSync('./src/assets/contact.json', '{"ActiveContacts":' + mapData + '}', err => {
         if (err) {
             console.log('Error writing file', err)
         } else {
             console.log('Successfully wrote file')
         }
     });
+    // mapData = JSON.parse(results);
 });
+
+function addr_search(add)
+{
+    console.log(add);
+ var xmlhttp = new XMLHttpRequest();
+ var url = "https://nominatim.openstreetmap.org/search?format=json&limit=3&q=" + add;
+ var myArr;
+
+ xmlhttp.onreadystatechange = function()
+ {
+   if (this.readyState == 4 && this.status == 200)
+   {
+     const temp = JSON.parse(this.responseText);
+     myArr = '[' + temp.lat + ',' + temp.lon + ']';
+   }
+ };
+ xmlhttp.open("GET", url, true);
+ xmlhttp.send();
+ return myArr;
+}
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
